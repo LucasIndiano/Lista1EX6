9cc88a7005a67f58d979bc36377923c8f2ccc838
ALUNO <ALUNO@LABFPC34>
2024-02-26 08:50:58 -0300

    Adicionar arquivos de projeto.

---
+++ Lista1EX6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1EX6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float num;
+            Console.WriteLine("Digite um Número: ");
+            num = float.Parse(Console.ReadLine());
+            float result = num % 4;
+            if (result == 0) { Console.WriteLine("Número Inválido"); }
+            else { Console.WriteLine("O Número " + num + " é Válido"); }
+            Console.ReadKey();  
+
+        }
+    }
+}
